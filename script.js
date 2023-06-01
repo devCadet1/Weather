239d0d2adf2676bb5f2ba65cc8aaaa600198f4f0
devCadet <satyam.karn3629@gmail.com>
2023-06-01 23:24:35 +0530

    Initial commit for weather app

---
+++ script.js
@@ -0,0 +1,9 @@
+function dark() {
+    document.getElementById("mood").style.backgroundColor = "#1f1f1f";
+    document.getElementById("mood").style.color = "#fff";
+}
+
+function light () {
+    document.getElementById('mood').style.backgroundColor = "#fff";
+    document.getElementById('mood').style.color = "#1f1f1f";
+}
