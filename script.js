9c2bfcf899b39d99af44292b0f058337528ccdb3
devCadet <satyam.karn3629@gmail.com>
2023-06-08 01:52:03 +0530

    Designed UI for weatherApp

--- script.js
+++
@@ -1,9 +0,0 @@
-function dark() {
-    document.getElementById("mood").style.backgroundColor = "#000";
-    document.getElementById("mood").style.color = "#fff";
-}
-
-function light () {
-    document.getElementById('mood').style.backgroundColor = "#fff";
-    document.getElementById('mood').style.color = "#1f1f1f";
-}
